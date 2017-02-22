17ed074df8b5b8eb26f17bf9496c2df11d6d08b0
1968155152@qq.com <1968155152@qq.com>
2017-02-22 19:25:07 +0800

    格式修改

--- KafkaDemo-1.0/src/main/java/com/wangl/KafkaConsumer.java
+++ KafkaDemo-1.0/src/main/java/com/wangl/KafkaConsumer.java
@@ -18,14 +18,12 @@ public class KafkaConsumer extends Thread {
     private final ConsumerConnector consumer;
     private final String topic;
 
-    public KafkaConsumer(String topic)
-    {
+    public KafkaConsumer(String topic) {
         consumer = kafka.consumer.Consumer.createJavaConsumerConnector(createConsumerConfig());
         this.topic = topic;
     }
 
-    private static ConsumerConfig createConsumerConfig()
-    {
+    private static ConsumerConfig createConsumerConfig() {
         Properties props = new Properties();
         props.put("zookeeper.connect", KafkaProperties.zkConnect);
         props.put("group.id", KafkaProperties.groupId);
@@ -38,10 +36,15 @@ public class KafkaConsumer extends Thread {
     @Override
     public void run() {
         Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
+
         topicCountMap.put(topic, new Integer(1));
+
         Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumer.createMessageStreams(topicCountMap);
+
         KafkaStream<byte[], byte[]> stream = consumerMap.get(topic).get(0);
+
         ConsumerIterator<byte[], byte[]> it = stream.iterator();
+
         while (it.hasNext()) {
             System.out.println("receive：" + new String(it.next().message()));
             try {
